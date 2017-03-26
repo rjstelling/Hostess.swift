b650be45b0d0983b764e6645cf6c95e72644d728
Richard Stelling <richard.stelling@naimaudio.com>
2017-03-26 23:14:13 +0100

    Header file tidy up.

--- Projects/Hostess/Hostess/Hostess.h
+++ Projects/Hostess/Hostess/Hostess.h
@@ -2,16 +2,16 @@
 //  Hostess.h
 //  Hostess
 //
-//  Created by Richard Stelling on 31/05/2016.
-//  Copyright © 2016 Richard Stelling. All rights reserved.
+//  Created by Richard James Stelling on 16/03/2017.
+//  Copyright © 2017 Richard Stelling. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-//! Project version number for Host.
+//! Project version number for Hostess.
 FOUNDATION_EXPORT double HostessVersionNumber;
 
-//! Project version string for Host.
+//! Project version string for Hostess.
 FOUNDATION_EXPORT const unsigned char HostessVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Hostess/PublicHeader.h>
